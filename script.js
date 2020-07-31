6776f4dd03d425e3c350c5b5a974b09216a14dec
Neha Hirve <neha.s.hirve@gmail.com>
2020-07-31 18:06:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const chilli = document.getElementById('splash-image')
+const splash = document.getElementById('splash')
+const page = document.getElementById('post-splash')
+const members = Array.from(document.getElementsByClassName('member'))
+const logo = document.getElementById('homepage')
+
+
+function splashOut () {
+  splash.style.display = 'none'
+  page.setAttribute('id', 'post-splash-visible')
+}
+
+function reveal (event) {
+  let member = event.target
+  let front = member.getElementsByClassName('front')[0]
+  let back = member.getElementsByClassName('back')[0]
+  front.className = 'back'
+  back.className = 'front'
+}
+
+function homePage () {
+  window.location.href = 'about.html'
+}
+
+if (chilli) {
+  chilli.addEventListener('click', splashOut)
+}
+
+members.forEach(portrait => portrait.addEventListener('mouseenter', reveal))
+members.forEach(portrait => portrait.addEventListener('mouseleave', reveal))
+
+logo.addEventListener('click', homePage)
