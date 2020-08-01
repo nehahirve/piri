216b6951b4baa2c16932133f2f4b1f8b14728edf
Neha Hirve <neha.s.hirve@gmail.com>
2020-08-01 13:07:39 +0530

    added cookies, fixed svg issue in safari, added media queries, fixed styling issues

--- script.js
+++ script.js
@@ -2,25 +2,33 @@ const chilli = document.getElementById('splash-image')
 const splash = document.getElementById('splash')
 const page = document.getElementById('post-splash')
 const members = Array.from(document.getElementsByClassName('member'))
-const logo = document.getElementById('homepage')
 
+// remove splash image
 
 function splashOut () {
   splash.style.display = 'none'
   page.setAttribute('id', 'post-splash-visible')
-  //logo.setAttribute('xlink:href', '#')
 }
+
+// set cookies
+
+if (/(^|;)\s*visited=/.test(document.cookie)) {
+  console.log(document.cookie)
+  if (chilli) {
+    splashOut()
+  }
+} else {
+  document.cookie = 'visited=true; max-age=150'
+}
+
+// switch between two divs
 
 function reveal (event) {
-  let member = event.target
-  let front = member.getElementsByClassName('front')[0]
-  let back = member.getElementsByClassName('back')[0]
+  const member = event.target
+  const front = member.getElementsByClassName('front')[0]
+  const back = member.getElementsByClassName('back')[0]
   front.className = 'back'
   back.className = 'front'
-}
-
-function homePage () {
-  window.location.href = 'about.html'
 }
 
 if (chilli) {
@@ -29,5 +37,3 @@ if (chilli) {
 
 members.forEach(portrait => portrait.addEventListener('mouseenter', reveal))
 members.forEach(portrait => portrait.addEventListener('mouseleave', reveal))
-
-//logo.addEventListener('click', homePage)
